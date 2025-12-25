72a14e2e30e60c3369b891b8a4197f4e6d2108f5
dangbm22-cloud <dangbm22@uef.edu.vn>
2025-12-25 22:09:30 +0700

    Gán đường dẫn cho categories để dẫn đến trang lọc sản phẩm

--- EcommerceWeb/wwwroot/js/main.js
+++ EcommerceWeb/wwwroot/js/main.js
@@ -63,37 +63,29 @@
     /*-----------------------
         Categories Slider
     ------------------------*/
-    $(".categories__slider").owlCarousel({
-        loop: true,
-        margin: 0,
-        items: 4,
-        dots: false,
-        nav: true,
-        navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true,
-        responsive: {
-
-            0: {
-                items: 1,
-            },
-
-            480: {
-                items: 2,
-            },
-
-            768: {
-                items: 3,
-            },
-
-            992: {
-                items: 4,
+    $(document).ready(function () {
+        $(".categories__slider").owlCarousel({
+            rewind: true,            // chạy hết rồi quay lại đầu ngay
+            margin: 0,
+            items: 4,
+            dots: false,
+            nav: true,
+            navText: ["<span class='fa fa-angle-left'></span>", "<span class='fa fa-angle-right'></span>"],
+            smartSpeed: 600,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplayHoverPause: true,
+            responsive: {
+                0: { items: 1 },
+                480: { items: 2 },
+                768: { items: 3 },
+                992: { items: 4 }
             }
-        }
+        });
     });
+
+
+
 
 
     $('.hero__categories__all').on('click', function(){
